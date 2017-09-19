e5c65d73c570ca06c1527fe6e82aa49d8f93ee10
Hoang Chuong <chuonghoanghong@gmail.com>
2017-09-19 10:09:58 +0700

    d

--- resources/views/templates/contact_tpl.blade.php
+++ resources/views/templates/contact_tpl.blade.php
@@ -23,12 +23,12 @@
             <div class="col-xs-12 col-md-6 col-left">
                 <h4>Liên hệ với chúng tôi</h4>
                 <div class="map">
-                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.2489414140177!2d105.80718531497325!3d21.02272298600164!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab662650a811%3A0x290d35faf7d4d792!2zMyBDaMO5YSBMw6FuZywgTMOhbmcgVGjGsOG7o25nLCDEkOG7kW5nIMSQYSwgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1491615129746" width="100%" height="320" frameborder="0" style="border:0" allowfullscreen></iframe>
+                    {!!$setting->iframemap!!}
                 </div>
-                <h5>Cty CP Công Nghệ và Truyền Thông GCO</h5>
-                <p class="">Phòng 109 Nhà B, Số 3 Chùa Láng, phường Láng Thượng, quận Đống Đa, TP Hà Nội</p>
-                <p class="">Tel: (04)-3999.4882 | Fax: (04)-3999.4882</p>
-                <p class="">Email: yduockhanhlinh@gmail.com</p>
+                <h5>{{$setting->company}}</h5>
+                <p class="">{{$setting->address}}</p>
+                <p class="">Tel: {{$setting->phone}} | Fax: {{$setting->fax}}</p>
+                <p class="">Email: {{$setting->email}}</p>
             </div>
             <div class="col-xs-12 col-md-6 right-left">
                 <h4>Gửi phản hồi cho chúng tôi</h4>
